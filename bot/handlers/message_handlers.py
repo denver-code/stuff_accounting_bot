275d81cdd44ebcf6ef57f51b3ee0f06bcc08d7d4
Denver <csigorek@gmail.com>
2023-03-15 17:43:34 +0000

    barcode scanner, add by UPC picture

--- bot/handlers/message_handlers.py
+++ bot/handlers/message_handlers.py
@@ -1,7 +1,11 @@
+from io import BytesIO
 from aiogram import types, dispatcher, Dispatcher
+import cv2
+import numpy as np
 from bot.core.api import get, verify_token
+from bot.core.barcode_reader import retval_code
 from bot.core.redis_db import get_user, is_token_exist, set_user
-from bot.states.add import AddByUPC
+from bot.states.add import AddByUPC, AddByUPCPicture
 from bot.states.authorisation import AuthToken
 
 async def set_token_event(message: types.Message, state: dispatcher.FSMContext):
@@ -27,6 +31,28 @@ async def add_by_upc_event(message: types.Message, state: dispatcher.FSMContext)
 
 
 
+async def add_by_upc_picture_event(message: types.Message, state: dispatcher.FSMContext):
+    _message = await message.answer('Thank you!\nLet me moment to parse data from picture')
+
+    photo_io = BytesIO()
+    await message.photo[-1].download(destination_file=photo_io)
+    photo_io.seek(0)
+    img = cv2.imdecode(np.frombuffer(photo_io.read(), np.uint8), 1)
+    ret_code = retval_code(img)
+    if ret_code[0] != 200:
+        return await _message.edit_text(ret_code[1])
+    await _message.edit_text('Let me moment to check if we have information about this item.')    
+
+    _token = is_token_exist(str(message.from_id))
+    _request = get(f"/api/v1/private/items/upc/{ret_code[1]}", _token)
+    if _request.status_code != 200:
+        return await _message.edit_text('Looks like we can\'t get information about this item, please make sure you have a correct UPC') 
+       
+    await state.finish()
+
+    await _message.edit_text(f"{_request.json()['title']} has been added successfully")
+
+
 async def message_event(message: types.Message, state: dispatcher.FSMContext):
     _message = await message.answer('Hi!\nI\'ll check your request a soon as posible!')
     # 
@@ -48,6 +74,7 @@ You also can add new item using command /add")
 def setup(dp: Dispatcher):
     dp.register_message_handler(set_token_event, content_types=['text'], state=AuthToken.token)
     dp.register_message_handler(add_by_upc_event, content_types=['text'], state=AddByUPC.upc_code)
+    dp.register_message_handler(add_by_upc_picture_event, content_types=['photo'], state=AddByUPCPicture.upc_picture)
     dp.register_message_handler(message_event, content_types=['text'], state='*')
 
 #eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmUiOjE2ODAyODQ1NTIuMTY1MzE0LCJlbWFpbCI6ImNzaWdvcmVrQGdtYWlsLmNvbSJ9.c-qDfg_BAgd5WBLRYPtXyEaeHWNiuNAVfc3wLZ5qqg0
